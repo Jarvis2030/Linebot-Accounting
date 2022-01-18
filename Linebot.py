a57ace749a154bd31953a884bbd7bccc315551b6
Jarvis2020 <ylindc@connect.ust.hk>
2022-01-18 10:20:11 +0800

    Add Linebot.py

--- Linebot.py
+++ Linebot.py
@@ -11,11 +11,32 @@ import datetime
 from oauth2client.service_account import ServiceAccountCredentials as SAC
 
 GDriveJSON = 'Accountingkey.json' #json file that contain Google API private key
-Gspreadsheet = 'CashFlow' # google sheet name
+Gspreadsheet = 'Personal Accounting' # google sheet name
 Line_bot_api = LineBotApi('mGZ+2slcF0VcNjs1YvreNpOm3h4STfJqPUzTonl64akZ79ieFSZjgvHU/PFx44dNBbzfcL5odxlvtcMcDaPeriu2A7LD43fPhkrOHzcS4Y5MSBg6gmto7Jqv15fOuODPKc2NdPWWiR108i0ti4u1VwdB04t89/1O/w1cDnyilFU=') # Linebot customize Api
 handler = WebhookHandler('008021c29864de0a406566a9ad5e01de') # channel secret
 
 app = Flask(__name__)
+
+def gsheetedit(event,content1):
+    while True:
+        try:
+            scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+            key = SAC.from_json_keyfile_name(GDriveJSON, scope)
+            client = gspread.authorize(key)
+            sheet = client.open(Gspreadsheet).sheet1
+        except Exception:
+            Line_bot_api.reply_message(event.reply_token,TextSendMessage(text="無法連到google sheet"))
+            sys.exit(1)
+
+        textt=""
+        textt+= content1
+        content1 = json.dumps(datetime.datetime.now(), cls = DateTimeEncoder) + '\0' + content1
+        content = content1.split('\0')
+        if textt!="":
+                sheet.append_row(content)
+                Line_bot_api.reply_message(event.reply_token,TextSendMessage(text="紀錄成功"))
+                print('新增一列資料到試算表' ,Gspreadsheet)
+                return textt
 
 class DateTimeEncoder(json.JSONEncoder):
     def default(self, z):
@@ -39,27 +60,17 @@ def callback(): #checking if the input message is right
 def handle_message(event):
     Line_bot_api.reply_message(event.reply_token, TextSendMessage(text = event.message.text))'''
 @handler.add(MessageEvent, message = TextMessage)
-def google_sheet_message(event):
-   #無法自動取得群組成員(初次加入時手動輸入群組成員)
-    Line_bot_api.reply_message(event.reply_token,TextSendMessage(text="紀錄成功"))
-    pass
-    while True:
-        try:
-            scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-            key = SAC.from_json_keyfile_name(GDriveJSON, scope)
-            client = gspread.authorize(key)
-            sheet = client.open(Gspreadsheet).sheet1
-        except Exception:
-            Line_bot_api.reply_message(event.reply_token,TextSendMessage(text="無法連到google sheet"))
-            sys.exit(1)
-
-        textt=""
-        textt+= event.message.text
-        if textt!="":
-                sheet.append_row((json.dumps(datetime.datetime.now(), cls = DateTimeEncoder), textt))
-                print('新增一列資料到試算表' ,Gspreadsheet)
-                return textt
-
+def reply_message(event):
+   #個人記帳功能
+    text = event.message.text
+    content = text.split()
+    for i in range(5-len(content)+1):
+        content.append('NA')
+        #確保全部index都有內容
+    if content[0] == '記帳':
+        lists = content[1] + '\0' + content[2] + '\0' + content[3] + '\0' + content[4] 
+        gsheetedit(event, lists)
+        
 
 '''@handler.add(MessageEvent, message = TextMessage) #The text Message you got from Linbot
 def handle_msg(event):
@@ -69,12 +80,6 @@ def handle_msg(event):
         Line_bot_api.reply_message(event.reply_token, message) #get the group ID '''
     
     
-
-
-
-
-
-
 
 if __name__ == '__main__':
     app.run() 
